23efbef689ce0061658208fd06174f74a1c751b6
Tran Hoang Duy <duytrieudong@gmail.com>
2023-07-21 15:37:32 +0700

    init

---
+++ components/Nav.js
@@ -0,0 +1,24 @@
+//  links
+import { HiHome, HiUser, HiRectangleGroup, HiViewColumns, HiChatBubbleBottomCenterText, HiEnvelope } from 'react-icons/hi'
+const links = [
+  { name: 'home', path: '/', icon: <HiHome /> },
+  { name: 'about', path: '/about', icon: <HiUser /> },
+  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
+  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  {
+    name: 'testimonials',
+    path: '/testimonials',
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    icon: <HiEnvelope />,
+  },
+];
+
+const Nav = () => {
+  return <nav>nav</nav>;
+};
+
+export default Nav;
---
+++ pages/services/index.js
@@ -0,0 +1,36 @@
+import { RxCrop, RxDesktop, RxPencil2, RxReader, RxRocket } from 'react-icons/rx';
+
+export const serviceData = [
+  {
+    icon: <RxCrop />,
+    title: 'Branding',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+  },
+  {
+    icon: <RxPencil2 />,
+    title: 'Design',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+  },
+  {
+    icon: <RxDesktop />,
+    title: 'Development',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+  },
+  {
+    icon: <RxReader />,
+    title: 'Copywriting',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+  },
+  {
+    icon: <RxRocket />,
+    title: 'SEO',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+  }
+]
+
+
+const Services = () => {
+  return <div>Services</div>;
+};
+
+export default Services;
